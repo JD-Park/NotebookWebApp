24adf626281f293e41a683bdf89340f582fd35f6
JD-Park <86543480+JD-Park@users.noreply.github.com>
2024-02-04 11:44:19 -0600

    Added auth routes.

--- website/auth.py
+++ website/auth.py
@@ -2,3 +2,15 @@ from flask import Blueprint
 
 """Blueprint for the application that houses defined URLs."""
 auth = Blueprint('auth', __name__)
+
+@auth.route('/login')
+def login():
+    return "<h1>Login</h1>"
+
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+@auth.route('/signup')
+def signup():
+    return "<p>Sign up</p>"
